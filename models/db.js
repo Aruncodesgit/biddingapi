ca6897136212b5fadab0c6b28c3682b9deeddb36
arun_microexcel <Arun.K@microexcel.com>
2023-05-04 15:02:54 +0530

    db changes

--- models/db.js
+++ models/db.js
@@ -8,7 +8,7 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
-});
+},{useNewUrlParser: true});
 
 require('./user.model');   
 require('./bidding.model'); 
