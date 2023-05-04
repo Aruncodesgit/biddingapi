2d6bc1731d7c987ab394c8313df8a323033d1718
arun_microexcel <Arun.K@microexcel.com>
2023-05-04 15:38:19 +0530

    server cahnges

--- app.js
+++ app.js
@@ -85,9 +85,8 @@ module.exports = router;
 
  
 
-
-
-
+const PORT = process.env.PORT || 3000
+ 
 
 // start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
